e5f92f96c8078648b8cc079b0e818c2addf3498a
HuzefaMotorwala <huzefamotorwala5225@outlook.com>
2021-01-16 12:23:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
+import './style.css';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+    const[quotes,setQuotes]=React.useState([]);
+    const[randomQuote,setRandomQuote]=React.useState([]);
+
+    React.useEffect(()=>{
+        async function fetchData(){
+            const response= await fetch("https://type.fit/api/quotes")
+            const data=await response.json();
+
+            setQuotes(data);
+            let randindex=Math.floor(Math.random()*data.length);
+            setRandomQuote(data[randindex]);
+        }
+        fetchData();
+    },[]);
+
+
+    const getNewQuote=()=>{
+        let randindex=Math.floor(Math.random()*quotes.length);
+        setRandomQuote(quotes[randindex]);
+    }
+
+    return(
+        <div>
+            <div id="quote-box">
+                <div id="quote-text">
+                    <i className="fas fa-quote-left">  <span id="text">
+                        {randomQuote ? (
+                            <>
+                            {randomQuote.text}
+                            </>
+                        ):(
+                            <h2>Loading...</h2>
+                        )}
+                        </span>
+                    </i>
+                </div>
+                <div id="quote-author"><span id="author">-{randomQuote.author||"no author"}</span></div>
+                <div className="buttons">
+                    <a href="https://twitter.com/" id="tweet-quote" class="button" title="post this tweet">
+                    <i className="fab fa-twitter"></i>
+                    </a>
+                    <button id="new-quote" className="button" onClick={getNewQuote}>New Qoute</button>
+                </div>
+            </div>
+       </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));
+
